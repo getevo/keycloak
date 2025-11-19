f37821858d86b2a68175ecf2caf4bbf13ea23d84
getevo <info@getevo.com>
2025-11-19 12:15:35 +0100

    Handle additional data types in `sprint` function for improved formatting.

--- keycloak.go
+++ keycloak.go
@@ -134,7 +134,7 @@ func setFieldValues(user interface{}, keycloakUser *UserInstance) (error, string
 		}
 		if chunks[0] == "attribute" {
 			//r = userRef.FieldByName(field.Name)
-			fmt.Println(chunks[1], "=", r.Interface(), ">>", sprint(r))
+			fmt.Println(chunks[1], "=", r.Interface(), ">>", sprint(r), "invalid?", !r.IsValid() || r.IsZero() || (r.Kind() == reflect.Ptr && r.IsNil()) || r.Interface() == nil)
 			if !r.IsValid() || r.IsZero() || (r.Kind() == reflect.Ptr && r.IsNil()) || r.Interface() == nil {
 				keycloakUser.Attributes.Set(chunks[1], "")
 				continue
